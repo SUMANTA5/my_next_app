0891144cf74c49ef88408752bd95f69b3725b5b0
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-09-22 13:43:15 +0530

    all com

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,7 +5,7 @@ function Home() {
         <div>
             <section class="text-gray-400 body-font">
                 <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-col text-center w-full mb-20">
+                    <div class="flex flex-col text-center w-full ">
                         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-black">Master Cleanse Reliac Heirloom</h1>
                         <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
                     </div>
